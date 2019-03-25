4769ff2d6a66944f778b6e5b4a768b9fe241fdcd
MostBest <chernov@mostbest.ru>
2019-03-25 17:47:06 +0300

    Add scripts

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,5 +1,85 @@
-//screen.width - 
-//window.innerWidth - 846h x 412w  ширина видимой области в css пиксилях 411 ppi 3 раза
-// 740х360
+// jQuery старт
+$(document).ready(function(){
 
-//alert( screen.height );
+function printError() {
+	var login 				= $('#login'),
+			password  		= $('#password'),
+			errorMessage 	= {
+				login: 		'<span id="login-error" class="login-error">Введите данные</span>',
+				password: '<span id="password-error" class="password-error">Укажите пароль</span>'
+			};
+
+	if (login.val() == '' && password.val() == '') {
+		login.addClass('error');
+		// login.insertAfter(errorMessage.login);
+		if ( $('.login-error').length == 0 ) {
+			setTimeout(function(){
+				login.focus();
+				$(errorMessage.login).insertAfter(login);
+			}, 200);
+		}
+	} else if (login.val() && password.val() == '') {
+		password.addClass('error');
+		if ( $('.password-error').length == 0  ) {
+			setTimeout(function(){
+				$(errorMessage.password).insertAfter(password);
+			}, 200);			
+		}
+	}
+}
+
+$('input').keypress(function() {
+	if ( $(this).val() ) {
+		$(this).removeClass('error');
+		$(this).siblings('span').remove();
+	}
+});
+
+$( '.form' ).submit( function( e ) {
+	printError();
+	e.preventDefault();
+});
+
+});
+// jQuery конец
+
+// ванильный js старт
+'use strict';
+
+/*for (var i = 0; i < 10; i++ ) {
+	for (var j = 0; j < i; j++ ) {
+		
+	}
+}*/
+
+/*window.onload = function() {
+
+	function addError() {
+		var login 				= document.getElementById('login'),
+				password  		= document.getElementById('password'),
+				loginErr 			= document.getElementById('login-error'),
+				errorMessage 	= {
+					login: 		'<span id="login-error" class="login-error">Введите данные</span>',
+					password: '<span id="password-error" class="password-error">Укажите пароль</span>'
+				};
+		if ( !login.value && !password.value) {
+			login.classList.add('input-error');
+			if ( loginErr ) {
+				setTimeout(function(){
+					login.focus();
+					//$(errorMessage.login).(login);
+				}, 200);
+			}
+		}
+	} 
+
+	function inserError() {
+
+	}
+
+	document.getElementById('form').addEventListener('submit', function(event){
+		addError();
+    event.preventDefault();
+  });
+}*/
+// ванильный js конец
--- gulp/tasks/script.js
+++ gulp/tasks/script.js
@@ -1,7 +1,8 @@
 module.exports = function () {
 	$.gulp.task('script:lib', function() {
 		return $.gulp.src([
-				'./dev/static/libs/slick/slick.min.js',
+				'./dev/static/libs/jquery/jquery-3.3.1.min.js',
+				'./dev/static/libs/slick/slick.min.js'
 			])
 			.pipe($.gp.concat('libs.min.js'))
 			.pipe($.gulp.dest('./build/assets/js'))
