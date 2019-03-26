1c8f14538cb68821b34761b276d8f2d07df0849c
MostBest <chernov@mostbest.ru>
2019-03-26 18:27:13 +0300

    JS error in form

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -9,24 +9,55 @@ function printError() {
 				password: '<span id="password-error" class="password-error">Укажите пароль</span>'
 			};
 
-	if (login.val() == '' && password.val() == '') {
-		login.addClass('error');
-		// login.insertAfter(errorMessage.login);
-		if ( $('.login-error').length == 0 ) {
-			setTimeout(function(){
-				login.focus();
-				$(errorMessage.login).insertAfter(login);
-			}, 200);
-		}
-	} else if (login.val() && password.val() == '') {
-		password.addClass('error');
-		if ( $('.password-error').length == 0  ) {
-			setTimeout(function(){
-				$(errorMessage.password).insertAfter(password);
-			}, 200);			
-		}
+	var x = '';
+	switch(x) {
+
+		case login.val():
+			login.addClass('error');
+			login.focus();
+			if ( $('.login-error').length == 0 ) {
+				setTimeout(function() {
+					$(errorMessage.login).insertAfter(login);
+				}, 200);
+			}
+			break;
+
+		case password.val():
+			password.addClass('error');
+			if ( $('.password-error').length == 0 ) {
+				setTimeout(function(){
+					$(errorMessage.password).insertAfter(password);
+				}, 200);			
+			}
+		default:
+			passwordControl();
 	}
 }
+
+function passwordControl() {
+	var password = $('#password');
+
+	if ( password.val().length < 8 ) {
+		$('.form-message').addClass('error');
+	}
+}
+
+
+/*
+setInterval(function() {
+	if ($('#login').val() && $('#password').val() && $('#password').val().length >= 8) {
+		$('.input-submit').removeClass('disabled');
+	} else {
+		$('.input-submit').not('.disabled').addClass('disabled');
+	}
+}, 500);
+*/
+
+
+			if ( password.val().length < 8) {
+				console.log( password.val().length );
+				$('.form-message').addClass('error');
+			}
 
 $('input').keypress(function() {
 	if ( $(this).val() ) {
@@ -36,50 +67,11 @@ $('input').keypress(function() {
 });
 
 $( '.form' ).submit( function( e ) {
-	printError();
-	e.preventDefault();
+	if ( $('.input-submit').hasClass('disabled') ) {
+		e.preventDefault();
+		// решить проблему с отправкой формы
+	}
 });
 
 });
 // jQuery конец
-
-// ванильный js старт
-'use strict';
-
-/*for (var i = 0; i < 10; i++ ) {
-	for (var j = 0; j < i; j++ ) {
-		
-	}
-}*/
-
-/*window.onload = function() {
-
-	function addError() {
-		var login 				= document.getElementById('login'),
-				password  		= document.getElementById('password'),
-				loginErr 			= document.getElementById('login-error'),
-				errorMessage 	= {
-					login: 		'<span id="login-error" class="login-error">Введите данные</span>',
-					password: '<span id="password-error" class="password-error">Укажите пароль</span>'
-				};
-		if ( !login.value && !password.value) {
-			login.classList.add('input-error');
-			if ( loginErr ) {
-				setTimeout(function(){
-					login.focus();
-					//$(errorMessage.login).(login);
-				}, 200);
-			}
-		}
-	} 
-
-	function inserError() {
-
-	}
-
-	document.getElementById('form').addEventListener('submit', function(event){
-		addError();
-    event.preventDefault();
-  });
-}*/
-// ванильный js конец
